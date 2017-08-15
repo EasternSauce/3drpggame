5f3670c039bac0ab2636833a6970430c8287da5e
Kamil Kurp <kam.kurp@gmail.com>
2017-08-15 19:47:47 +0200

    init commit, gravity, orbiting camera

---
+++ src/main/java/org/lwjglb/engine/items/GameItem.java
@@ -0,0 +1,174 @@
+package org.lwjglb.engine.items;
+
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
+import org.lwjglb.engine.graph.Mesh;
+
+public class GameItem {
+
+    private boolean selected;
+
+    private Mesh[] meshes;
+
+    private final Vector3f position;
+
+    private float scale;
+
+    private final Quaternionf rotation;
+
+    private int textPos;
+    
+    private boolean disableFrustumCulling;
+
+    private boolean insideFrustum;
+
+	private float verticalVelocity;
+    private float verticalAccel;
+
+    public GameItem() {
+        selected = false;
+        position = new Vector3f(0, 0, 0);
+        scale = 1;
+        rotation = new Quaternionf();
+        textPos = 0;
+        insideFrustum = true;
+        disableFrustumCulling = false;
+        verticalVelocity = 0.0f;
+        verticalAccel = -0.07f;
+    }
+
+    public GameItem(Mesh mesh) {
+        this();
+        this.meshes = new Mesh[]{mesh};
+    }
+
+    public GameItem(Mesh[] meshes) {
+        this();
+        this.meshes = meshes;
+    }
+
+    public Vector3f getPosition() {
+        return position;
+    }
+
+    public int getTextPos() {
+        return textPos;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public final void setPosition(float x, float y, float z) {
+        this.position.x = x;
+        this.position.y = y;
+        this.position.z = z;
+    }
+    
+    public void movePosition(float offsetX, float offsetY, float offsetZ) {
+    	//Vector3f euler = rotation.getEulerAnglesXYZ(new Vector3f());
+    	//euler.y += Math.PI / 2;
+    	//System.out.println(euler.y);
+//        if ( offsetZ != 0 ) {
+//            position.x += (float)Math.sin(angle) * -1.0f * offsetZ;
+//            position.z += (float)Math.cos(angle) * offsetZ;
+//        }
+//        if ( offsetX != 0) {
+//            position.x += (float)Math.sin(angle - 90) * -1.0f * offsetX;
+//            position.z += (float)Math.cos(angle - 90) * offsetX;
+//        }
+//        position.y += offsetY;
+        
+        this.position.x += offsetX;
+        this.position.y += offsetY;
+        this.position.z += offsetZ;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public final void setScale(float scale) {
+        this.scale = scale;
+    }
+
+    public Quaternionf getRotation() {
+        return rotation;
+    }
+
+    public final void setRotation(Quaternionf q) {
+        this.rotation.set(q);
+    }
+
+    public Mesh getMesh() {
+        return meshes[0];
+    }
+
+    public Mesh[] getMeshes() {
+        return meshes;
+    }
+
+    public void setMeshes(Mesh[] meshes) {
+        this.meshes = meshes;
+    }
+
+    public void setMesh(Mesh mesh) {
+        this.meshes = new Mesh[]{mesh};
+    }
+
+    public void cleanup() {
+        int numMeshes = this.meshes != null ? this.meshes.length : 0;
+        for (int i = 0; i < numMeshes; i++) {
+            this.meshes[i].cleanUp();
+        }
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public void setTextPos(int textPos) {
+        this.textPos = textPos;
+    }
+
+    public boolean isInsideFrustum() {
+        return insideFrustum;
+    }
+
+    public void setInsideFrustum(boolean insideFrustum) {
+        this.insideFrustum = insideFrustum;
+    }
+    
+    public boolean isDisableFrustumCulling() {
+        return disableFrustumCulling;
+    }
+
+    public void setDisableFrustumCulling(boolean disableFrustumCulling) {
+        this.disableFrustumCulling = disableFrustumCulling;
+    }
+
+	public float getVerticalVelocity() {
+		return verticalVelocity;
+	}
+
+	public void setVerticalVelocity(float verticalVelocity) {
+		this.verticalVelocity = verticalVelocity;
+	}
+
+	public float getVerticalAccel() {
+		return verticalAccel;
+	}
+
+	public void setVerticalAccel(float verticalAccel) {
+		this.verticalAccel = verticalAccel;
+	}
+	
+	public void jump() {
+		if(position.y <= 0.0f) {
+			this.verticalVelocity = 1.3f;
+
+		}
+	}
+    
+    
+}
---
+++ src/main/java/org/lwjglb/game/DummyGame.java
@@ -0,0 +1,273 @@
+package org.lwjglb.game;
+
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_A;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_D;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_LEFT;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_RIGHT;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_S;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_W;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_X;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_Z;
+
+import java.io.File;
+
+import org.joml.Quaternionf;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+import org.lwjglb.engine.IGameLogic;
+import org.lwjglb.engine.MouseInput;
+import org.lwjglb.engine.Scene;
+import org.lwjglb.engine.SceneLight;
+import org.lwjglb.engine.Window;
+import org.lwjglb.engine.graph.Camera;
+import org.lwjglb.engine.graph.Mesh;
+import org.lwjglb.engine.graph.Renderer;
+import org.lwjglb.engine.graph.anim.AnimGameItem;
+import org.lwjglb.engine.graph.anim.Animation;
+import org.lwjglb.engine.graph.lights.DirectionalLight;
+import org.lwjglb.engine.graph.weather.Fog;
+import org.lwjglb.engine.items.GameItem;
+import org.lwjglb.engine.items.SkyBox;
+import org.lwjglb.engine.loaders.assimp.AnimMeshesLoader;
+import org.lwjglb.engine.loaders.assimp.StaticMeshesLoader;
+
+public class DummyGame implements IGameLogic {
+
+    private static final float MOUSE_SENSITIVITY = 0.2f;
+
+    private final Vector3f posInc;
+
+    private final Renderer renderer;
+
+    private final Camera camera;
+
+    private Scene scene;
+
+    private static final float CAMERA_POS_STEP = 0.40f;
+
+    private float angleInc;
+
+    private float lightAngle;
+
+    private boolean firstTime;
+
+    private boolean sceneChanged;
+
+    private Animation animation;
+
+    private AnimGameItem animItem;
+
+    private GameItem[] gameItems;
+    
+    private Vector2f lookAt = new Vector2f(0.0f, 0.0f);
+    
+    private float camDistance = 2.0f;
+
+    private float previousScroll = 0.0f;
+    
+    public DummyGame() {
+        renderer = new Renderer();
+        camera = new Camera();
+        posInc = new Vector3f(0.0f, 0.0f, 0.0f);
+        angleInc = 0;
+        lightAngle = 90;
+        firstTime = true;
+    }
+
+    @Override
+    public void init(Window window) throws Exception {
+        renderer.init(window);
+
+        scene = new Scene();
+
+        String fileName = Thread.currentThread().getContextClassLoader()
+                .getResource("models/terrain/terrain.obj").getFile();
+        File file = new File(fileName);
+        Mesh[] terrainMesh = StaticMeshesLoader.load(file.getAbsolutePath(), "/models/terrain");
+        GameItem terrain = new GameItem(terrainMesh);
+        terrain.setScale(100.0f);
+
+        fileName = Thread.currentThread().getContextClassLoader()
+                .getResource("models/bob/boblamp.md5mesh").getFile();
+        file = new File(fileName);
+        animItem = AnimMeshesLoader.loadAnimGameItem(file.getAbsolutePath(), "");
+        animItem.setScale(0.05f);
+        animation = animItem.getCurrentAnimation();
+        
+        fileName = Thread.currentThread().getContextClassLoader()
+                .getResource("models/house/house.obj").getFile();
+        file = new File(fileName);
+        Mesh[] houseMesh = StaticMeshesLoader.load(file.getAbsolutePath(), "/models/house");
+        GameItem house = new GameItem(houseMesh);
+        house.setScale(0.5f);
+        house.setPosition(10.0f, 0.0f, 10.0f);
+        
+        scene.setGameItems(new GameItem[]{animItem, terrain, house});
+
+        // Shadows
+        scene.setRenderShadows(true);
+
+        // Fog
+        Vector3f fogColour = new Vector3f(0.5f, 0.5f, 0.5f);
+        scene.setFog(new Fog(true, fogColour, 0.02f));
+
+        // Setup  SkyBox
+        float skyBoxScale = 100.0f;
+        fileName = Thread.currentThread().getContextClassLoader()
+                .getResource("models/skybox.obj").getFile();
+        file = new File(fileName);
+        SkyBox skyBox = new SkyBox(file.getAbsolutePath(), new Vector4f(0.65f, 0.65f, 0.65f, 1.0f));
+        skyBox.setScale(skyBoxScale);
+        scene.setSkyBox(skyBox);
+
+        // Setup Lights
+        setupLights();
+
+        camera.getPosition().x = -1.5f;
+        camera.getPosition().y = 3.0f;
+        camera.getPosition().z = 4.5f;
+        camera.getRotation().x = 15.0f;
+        camera.getRotation().y = 390.0f;
+    }
+
+    private void setupLights() {
+        SceneLight sceneLight = new SceneLight();
+        scene.setSceneLight(sceneLight);
+
+        // Ambient Light
+        sceneLight.setAmbientLight(new Vector3f(0.3f, 0.3f, 0.3f));
+        sceneLight.setSkyBoxLight(new Vector3f(1.0f, 1.0f, 1.0f));
+
+        // Directional Light
+        float lightIntensity = 1.0f;
+        Vector3f lightDirection = new Vector3f(0, 1, 1);
+        DirectionalLight directionalLight = new DirectionalLight(new Vector3f(1, 1, 1), lightDirection, lightIntensity);
+        sceneLight.setDirectionalLight(directionalLight);
+    }
+
+    @Override
+    public void input(Window window, MouseInput mouseInput) {
+        sceneChanged = false;
+        boolean moved = false;
+        posInc.set(0, 0, 0);
+        if (window.isKeyPressed(GLFW_KEY_W)) {
+            sceneChanged = true;
+            posInc.z = -1;
+            moved = true;
+        } else if (window.isKeyPressed(GLFW_KEY_S)) {
+            sceneChanged = true;
+            posInc.z = 1;
+            moved = true;
+        }
+        if (window.isKeyPressed(GLFW_KEY_A)) {
+            sceneChanged = true;
+            posInc.x = -1;
+            moved = true;
+        } else if (window.isKeyPressed(GLFW_KEY_D)) {
+            sceneChanged = true;
+            posInc.x = 1;
+            moved = true;
+        }
+        if (window.isKeyPressed(GLFW_KEY_Z)) {
+            sceneChanged = true;
+            posInc.y = -1;
+            moved = true;
+        } else if (window.isKeyPressed(GLFW_KEY_X)) {
+            sceneChanged = true;
+            posInc.y = 1;
+            moved = true;
+        }
+        if (window.isKeyPressed(GLFW_KEY_LEFT)) {
+            sceneChanged = true;
+            angleInc -= 0.05f;
+        } else if (window.isKeyPressed(GLFW_KEY_RIGHT)) {
+            sceneChanged = true;
+            angleInc += 0.05f;
+        } else {
+            angleInc = 0;            
+        }
+        if (window.isKeyPressed(GLFW_KEY_SPACE)) {
+            sceneChanged = true;
+            animItem.jump();
+            //animation.nextFrame();
+        }
+        if(moved) {
+        	animation.nextFrame();
+        }
+    }
+
+    @Override
+    public void update(float interval, MouseInput mouseInput, Window window) {
+    	sceneChanged = true;
+        if (mouseInput.isRightButtonPressed()) {
+            // Update camera based on mouse            
+            Vector2f rotVec = mouseInput.getDisplVec();
+            //camera.moveRotation(rotVec.x * MOUSE_SENSITIVITY, rotVec.y * MOUSE_SENSITIVITY, 0);
+            //sceneChanged = true;
+            lookAt.x +=rotVec.x * MOUSE_SENSITIVITY;
+            lookAt.y +=rotVec.y * MOUSE_SENSITIVITY;
+        }
+
+        // Update camera position
+        //System.out.println((float)Math.cos(Math.toRadians(lookAt.y)));
+        if(animItem.getPosition().y + animItem.getVerticalVelocity() < 0.0f) {
+        	animItem.setPosition(animItem.getPosition().x, 0.0f, animItem.getPosition().z);
+        	animItem.setVerticalVelocity(0.0f);
+        }
+        
+        if(animItem.getVerticalVelocity() != 0.0f) {
+        	animItem.movePosition(0.0f, animItem.getVerticalVelocity(), 0.0f);
+            animItem.setVerticalVelocity(animItem.getVerticalVelocity() + animItem.getVerticalAccel());
+        }
+
+        animItem.movePosition(posInc.z * CAMERA_POS_STEP * (float)Math.cos(Math.toRadians(lookAt.y)) + posInc.x * CAMERA_POS_STEP * (float)Math.cos(Math.toRadians(-lookAt.y + 90.0f)), 
+        		posInc.y * CAMERA_POS_STEP,
+        		posInc.z * CAMERA_POS_STEP * (float)Math.sin(Math.toRadians(lookAt.y)) + posInc.x * CAMERA_POS_STEP * (float)Math.sin(Math.toRadians(lookAt.y - 90.0f)));
+        //camera.setPosition(animItem.getPosition().x, animItem.getPosition().y, animItem.getPosition().z);
+        
+        camera.setPosition(animItem.getPosition().x + camDistance * (float)Math.cos(Math.toRadians(lookAt.y)) * (float)Math.cos(Math.toRadians(lookAt.x)), 1.5f + animItem.getPosition().y + camDistance * (float)Math.sin(Math.toRadians(lookAt.x)), animItem.getPosition().z + camDistance * (float)Math.sin(Math.toRadians(lookAt.y)) * (float)Math.cos(Math.toRadians(lookAt.x)));
+        camera.setRotation(lookAt.x, 90.0f + lookAt.y + 180.0f, 0.0f);
+        animItem.setRotation(new Quaternionf().rotateY( (float)Math.toRadians(-90.0f - lookAt.y)));
+        
+        float scroll = mouseInput.getScroll();
+        float deltaScroll = previousScroll - scroll;
+        previousScroll = scroll;
+        
+        camDistance += deltaScroll * 0.04f;
+
+        lightAngle += angleInc;
+        if (lightAngle < 0) {
+            lightAngle = 0;
+        } else if (lightAngle > 180) {
+            lightAngle = 180;
+        }
+        float zValue = (float) Math.cos(Math.toRadians(lightAngle));
+        float yValue = (float) Math.sin(Math.toRadians(lightAngle));
+        Vector3f lightDirection = this.scene.getSceneLight().getDirectionalLight().getDirection();
+        lightDirection.x = 0;
+        lightDirection.y = yValue;
+        lightDirection.z = zValue;
+        lightDirection.normalize();
+
+        // Update view matrix
+        camera.updateViewMatrix();
+    }
+
+    @Override
+    public void render(Window window) {
+        if (firstTime) {
+            sceneChanged = true;
+            firstTime = false;
+        }
+        renderer.render(window, camera, scene, sceneChanged);
+    }
+
+    @Override
+    public void cleanup() {
+        renderer.cleanup();
+
+        scene.cleanup();
+    }
+}
